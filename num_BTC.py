d6cfdc7a869e7f4b11a75c858b4c42d99a2d6536
cleofan <58447038+cleofan@users.noreply.github.com>
2022-02-28 16:45:19 -0500

    Update num_BTC.py

--- num_BTC.py
+++ num_BTC.py
@@ -3,8 +3,16 @@ import math
 def num_BTC(b):
     c = float(0)
     #Document the number of halving events
-    n_halves = b // 210000;
-    c = 50 * (0.5) ** n_halves * b;
+    n_halves = b // 210000
+    #Document the initial reward per block
+    rewardPerBlock = 50
+    while(n_halves > 0):
+        c = c + rewardPerBlock * 210000
+        n_halves--
+        rewardPerBlock = rewardPerBlock * 0.5
+    
+    c = c + (b % 210000) * rewardPerBlock
+    
     return c
 
 
